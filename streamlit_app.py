c92ff651faa1b3a4786bd7f950e2f0e68b25500b
Kaothar Adedeji <132794549+Kaothar2@users.noreply.github.com>
2025-02-18 20:43:42 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,8 +51,10 @@ if st.button("Predict Price"):
     input_data[main_gem] = 1  # Set the selected gem to 1
 
     # Scale numerical features
-    input_data[["Year", "Month"]] = scaler.transform(input_data[["Year", "Month"]])
-
+       numerical_features = input_data[["Year", "Month"]]
+       scaled_features = scaler.transform(numerical_features)  # Transform numerical features
+       input_data[["Year", "Month"]] = scaled_features  # Update the original DataFrame
+   
     # Reorder columns to match training data
     input_data = input_data[one_hot_columns[:-1]]  # Exclude Price_USD
 
